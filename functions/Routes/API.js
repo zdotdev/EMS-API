2d3fdb8b8e11c6078e7c76036584df353ed30563
zdotdev <www.ragaszedrick25@gmail.com>
2024-05-22 23:14:18 +0800

    added get by businessName

--- functions/Routes/API.js
+++ functions/Routes/API.js
@@ -23,6 +23,15 @@ router.get('/', async (req, res) => {
     res.status(500).json({ message: err.message })
   }
 })
+router.get('/:businessName', async (req, res) => {
+  try {
+    const order = await orderModel.findOne()
+    res.status(200).json(order)
+  } catch (err) {
+    res.status(500).json({ message: err.message })
+  }
+})
+
 router.get('/:id', getOrder, (req, res) => {
   try {
     res.json(res.order)

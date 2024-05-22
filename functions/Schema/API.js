6f78ef5966bcee61f4add143a52c94d4b6b7474c
zdotdev <www.ragaszedrick25@gmail.com>
2024-05-22 20:49:41 +0800

    removed order number

--- functions/Schema/API.js
+++ functions/Schema/API.js
@@ -2,11 +2,6 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const orderSchema = new Schema({
-  orderNumber: {
-    type: Schema.Types.ObjectId,
-    ref: 'Order',
-    required: true
-  },
   orderDate: {
     type: Date,
     required: true,

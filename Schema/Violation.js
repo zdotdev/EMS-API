b83e539feb57d2a9af9b318d280c8d0053e744b5
zdotdev <www.ragaszedrick25@gmail.com>
2024-08-12 21:54:24 +0800

    added Violations schema

---
+++ Schema/Violation.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose'
+
+const ViolationSchema = new mongoose.Schema({
+  business: { type: mongoose.Schema.Types.ObjectId, ref: 'Business' },
+  violationName: { type: String, required: true },
+  violationFee: { type: String, required: true },
+  violationDescription: { type: String, required: true },
+  violationImage: { type: String, required: true },
+  isArchived: { type: Boolean, default: false },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now }
+})
+
+export default mongoose.model('Violation', ViolationSchema)
